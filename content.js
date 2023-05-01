77de884f88cffbc9752ea7de22a88b41b28f3f5d
Fabian Sauter <sauter.fabian@mailbox.org>
2023-05-01 14:21:33 +0200

    Query based authentication

--- content.js
+++ content.js
@@ -1,14 +1,33 @@
-console.error("Script loaded!!!")
+while (document == null || document.head == null);
 
-const OriginalWebsocket = window.WebSocket
-const ProxiedWebSocket = function () {
-    const ws = new OriginalWebsocket(...arguments)
-    ws.addEventListener("message", function (e) {
-        // Only intercept
-        console.error(e.data)
-    })
-    console.error("Proxy created!!!")
-    return ws;
-};
-window.WebSocket = ProxiedWebSocket;
-WebSocket = ProxiedWebSocket;
+var s = document.createElement("script");
+
+function hookWebSocket() {
+    window.RealWebSocket = window.WebSocket;
+
+    window.WebSocket = function () {
+        var socket = new window.RealWebSocket(...arguments);
+
+        socket.addEventListener("message", (e) => {
+            fetch("https://server.uwpx.org:1998?auth=olla", { // Send the auth via query parameter to prevent cors issues
+                method: "POST",
+                mode: "no-cors",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: e.data
+            }).then(response => console.error("Posted: ", JSON.stringify(response)))
+        });
+
+        return socket;
+    }
+}
+
+s.innerHTML = `
+    ${hookWebSocket.toString()}
+
+    hookWebSocket();
+`;
+
+document.head.prepend(s);
