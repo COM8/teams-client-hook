bc1275e8308bcc37375ea73854d7d25ad8abdd9a
Fabian Sauter <sauter.fabian@mailbox.org>
2023-07-08 14:50:16 +0200

    Posting activity badge count

--- content.js
+++ content.js
@@ -2,6 +2,25 @@ while (document == null || document.head == null);
 
 var s = document.createElement("script");
 
+// Send message directly without first sending a chrome.runtime.sendMessage to the background
+function sendToBackground(data) {
+    chrome.storage.local.get(["token"], function (token) {
+        chrome.storage.local.get(["url"], function (url) {
+            fetchUrl = url["url"] + "?auth=" + token["token"];
+            console.debug("[Teams Client] Send to: ", fetchUrl);
+            fetch(fetchUrl, { // Send the auth via query parameter to prevent cors issues
+                method: "POST",
+                mode: "no-cors",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: data
+            }).then(response => console.debug("[Teams Client] Posted: ", JSON.stringify(data)));
+        });
+    });
+}
+
 function hookWebSocket() {
     window.RealWebSocket = window.WebSocket;
 
@@ -10,13 +29,11 @@ function hookWebSocket() {
 
         socket.addEventListener("message", (e) => {
             // We need to send a message to our extension since in an injected script, we do not have access to chrome.*
-            // Extension ID
-            var editorExtensionId = "fekcnofjiglkhlhiiodbpbedplghdkfp";
-            // Make a simple request:
-            chrome.runtime.sendMessage(editorExtensionId, { action: "getTokenUrl" },
+            var extensionID = "fekcnofjiglkhlhiiodbpbedplghdkfp";
+            chrome.runtime.sendMessage(extensionID, { action: "getTokenUrl" },
                 function (response) {
-                    fetchUrl = response.url + "?auth=" + response.token
-                    console.log("Send to: ", fetchUrl)
+                    fetchUrl = response.url + "?auth=" + response.token;
+                    console.debug("[Teams Client] Send to: ", fetchUrl);
                     fetch(fetchUrl, { // Send the auth via query parameter to prevent cors issues
                         method: "POST",
                         mode: "no-cors",
@@ -25,7 +42,7 @@ function hookWebSocket() {
                             "Content-Type": "application/json"
                         },
                         body: e.data
-                    }).then(response => console.error("Posted: ", JSON.stringify(response)))
+                    }).then(response => console.debug("[Teams Client] Posted: ", JSON.stringify(response)));
                 });
         });
 
@@ -40,3 +57,29 @@ s.innerHTML = `
 `;
 
 document.head.prepend(s);
+
+
+setInterval(function () {
+    let targetNodes = document.querySelectorAll('.activity-badge');
+    if (targetNodes.length <= 0) {
+        return
+    }
+
+    let sum = 0;
+
+    targetNodes.forEach(function (node) {
+        // If the node also has the 'activity-badge-container' class, skip it
+        if (node.classList.contains('activity-badge-container')) {
+            return;
+        }
+
+        let value = parseInt(node.textContent.trim(), 10);
+        if (!isNaN(value)) { // make sure the content was a number
+            sum += value;
+        }
+    });
+
+    let activityObject = { activity: { count: sum } };
+    sendToBackground(JSON.stringify(activityObject));
+
+}, 5000); // 5000 milliseconds = 5 seconds
